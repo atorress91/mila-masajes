1aea917ddb395a4da18930c5cf14b5abfd15f9a6
AndresTS <andres91411@gmail.com>
2026-01-11 10:40:43 -0600

    chore: update dependencies in package.json
    - upgraded @astrojs/node to version 9.5.1
    - upgraded @astrojs/sitemap to version 3.6.1
    - upgraded astro to version 5.16.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://milamassage.com',
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   compressHTML: true,
 
   build: {
